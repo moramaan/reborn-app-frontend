554ae32a300ab230cc616d1dd215688b783753df
moramaan <dmorahidalgo@gmail.com>
2024-05-14 21:05:39 +0200

    Clean old code and MVP of home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,86 +30,44 @@ const jsonData = [
     img: "https://nextui.org/images/album-cover.png",
     price: "500€",
   },
-  {
-    title: "Curso de Next.js",
-    img: "https://nextui.org/images/album-cover.png",
-    price: "500€",
-  },
-  {
-    title: "Curso de Next.js",
-    img: "https://nextui.org/images/album-cover.png",
-    price: "500€",
-  },
-  {
-    title: "Curso de Next.js",
-    img: "https://nextui.org/images/album-cover.png",
-    price: "500€",
-  },
-  {
-    title: "Curso de Next.js",
-    img: "https://nextui.org/images/album-cover.png",
-    price: "500€",
-  },
 ];
 
 export default function IndexPage() {
   return (
     <DefaultLayout>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-12">
+      <section className="flex flex-col items-center justify-center gap-4 py-4 md:py-8">
         <div className="inline-block max-w-2xl text-center justify-center">
-          <h2 className={title({ color: "violet" })}>M9 UF2 Proyecto Final</h2>
-          <h4 className={subtitle({ class: "mt-4" })}>
-            Construido con la librería{" "}
-            <span className="text-primary">Next UI</span>:
-          </h4>
-        </div>
-
-        <div className="flex gap-3">
-          <Link
-            isExternal
-            href={siteConfig.links.docs}
-            className={buttonStyles({
-              color: "primary",
-              radius: "full",
-              variant: "shadow",
-            })}
-          >
-            Documentación
-          </Link>
-          <Link
-            isExternal
-            className={buttonStyles({ variant: "bordered", radius: "full" })}
-            href={siteConfig.links.github}
-          >
-            <GithubIcon size={20} />
-            GitHub
-          </Link>
+          <h2 className={title({ color: "green" })}>DAW Proyecto Final</h2>
         </div>
       </section>
-      <section className="flex flex-col items-center justify-center py-4">
+      <section className="flex flex-col items-center justify-center py-2">
         <div className="max-w-5xl w-full text-center items-center justify-between font-mono text-sm sm:mt-5">
-          <h4 className={title({ color: "yellow" })}>Productos</h4>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {/* <ProductCard
-              title="Curso de Next.js"
-              img="https://nextui.org/images/album-cover.png"
-              price="500€"
-            />
-            <ProductCard
-              title="Curso de Next.js"
-              img="https://nextui.org/images/album-cover.png"
-              price="500€"
-            />
-            <ProductCard
-              title="Curso de Next.js"
-              img="https://nextui.org/images/album-cover.png"
-              price="500€"
-            />
-            <ProductCard
-              title="Curso de Next.js"
-              img="https://nextui.org/images/album-cover.png"
-              price="500€"
-            /> */}
+          <h4 className={`w-full text-start ${title({ color: "yellow" })}`}>Recomendados</h4>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+            {jsonData.map((data, index) => (
+              <ProductCard key={index} {...data} />
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className="flex flex-col items-center justify-center py-2">
+        <div className="max-w-5xl w-full text-center items-center justify-between font-mono text-sm sm:mt-5">
+          <h4 className={`w-full text-start ${title({ color: "yellow" })}`}>
+            Novedades
+          </h4>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+            {jsonData.map((data, index) => (
+              <ProductCard key={index} {...data} />
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className="flex flex-col items-center justify-center py-2">
+        <div className="max-w-5xl w-full text-center items-center justify-between font-mono text-sm sm:mt-5">
+          <h4 className={`w-full text-start ${title({ color: "yellow" })}`}>
+            Con más me gusta
+          </h4>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
             {jsonData.map((data, index) => (
               <ProductCard key={index} {...data} />
             ))}
