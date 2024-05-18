ddecd46ecaa9d460f9cfaec75ceb6fb8aad5a71a
moramaan <dmorahidalgo@gmail.com>
2024-05-18 17:01:28 +0200

    Change product info design

--- components/icons.tsx
+++ components/icons.tsx
@@ -96,6 +96,39 @@ export const MessageIcon: React.FC<IconSvgProps> = ({
   </svg>
 );
 
+export const LocationIcon: React.FC<IconSvgProps> = ({
+  size = 24,
+  width,
+  height,
+  ...props
+}) => (
+  <svg
+    aria-hidden="true"
+    fill="none"
+    focusable="false"
+    height={size || height}
+    role="presentation"
+    viewBox="0 0 24 24"
+    width={size || width}
+    {...props}
+  >
+    <path
+      d="M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M12 13C13.6569 13 15 11.6569 15 10C15 8.34315 13.6569 7 12 7C10.3431 7 9 8.34315 9 10C9 11.6569 10.3431 13 12 13Z"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
+
 export const GithubIcon: React.FC<IconSvgProps> = ({
   size = 24,
   width,
--- components/product-info.tsx
+++ components/product-info.tsx
@@ -1,41 +1,45 @@
 import React from "react";
-import { Accordion, AccordionItem } from "@nextui-org/react";
+import { Card, CardBody, CardHeader } from "@nextui-org/react";
+import { LocationIcon } from "@/components/icons";
+
+interface ProductInfoProps {
+  price: number;
+  location: string;
+  published: string;
+  state: string;
+  description: string;
+}
 
 const defaultContent: string =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quasi soluta maiores reprehenderit ratione voluptatem officiis, repellendus ex quisquam error. Debitis vel itaque ex dolorem ab consequatur accusamus reiciendis nemo!";
 
-const ProductInfo: React.FC = () => {
+const ProductInfo: React.FC<ProductInfoProps> = ({
+  price = 500,
+  location = "Barcelona, Cataluña",
+  published = "2 días",
+  state = "Nuevo",
+  description = "",
+}) => {
   return (
-    <Accordion defaultExpandedKeys={["2"]}>
-      <AccordionItem
-        key="1"
-        aria-label="Accordion 1"
-        subtitle="Press to expand"
-        title="Accordion 1"
-      >
-        {defaultContent}
-      </AccordionItem>
-      <AccordionItem
-        key="2"
-        aria-label="Accordion 2"
-        subtitle={
-          <span>
-            Press to expand <strong>key 2</strong>
-          </span>
-        }
-        title="Accordion 2"
-      >
-        {defaultContent}
-      </AccordionItem>
-      <AccordionItem
-        key="3"
-        aria-label="Accordion 3"
-        subtitle="Press to expand"
-        title="Accordion 3"
-      >
-        {defaultContent}
-      </AccordionItem>
-    </Accordion>
+    <Card className="mt-2 h-[325px] px-2 py-2">
+      <CardHeader className="flex flex-col items-start">
+        <div className="flex flex-col items-start w-full">
+          <div className="flex justify-between w-full">
+            <h4 className="text-lg font-semibold">{price}€</h4>
+            <div className="flex items-center">
+              <LocationIcon />
+              <span className="pt-1">{location}</span>
+            </div>
+          </div>
+          <p className="text-sm text-default-600">Publicado hace {published}</p>
+          <p className="text-sm text-default-700">{state}</p>
+        </div>
+      </CardHeader>
+      <CardBody>
+        <h5 className="text-lg font-semibold">Descripción</h5>
+        <p>{description ? description : defaultContent}</p>
+      </CardBody>
+    </Card>
   );
 };
 
