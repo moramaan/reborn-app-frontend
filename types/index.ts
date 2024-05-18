fa3c30df6c2f85aebc4bd5386940c5766ae152af
moramaan <dmorahidalgo@gmail.com>
2024-05-18 21:14:39 +0200

    Add global Product interface

--- types/index.ts
+++ types/index.ts
@@ -3,3 +3,15 @@ import {SVGProps} from "react";
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
+
+export interface Product {
+  id: string;
+  title: string;
+  img: string;
+  price: number;
+  location: string;
+  published: string;
+  state: string;
+  description: string;
+  images: string[];
+}
