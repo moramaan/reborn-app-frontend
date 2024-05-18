0b092e9e173d635b5edadb19db768d76f60af803
moramaan <dmorahidalgo@gmail.com>
2024-05-18 17:19:04 +0200

    Add feature - carousel phone slide

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Card, CardHeader, Image, Button } from "@nextui-org/react";
 import { LeftArrow, RightArrow } from "./icons";
+import Swipe from "react-easy-swipe";
 
 const images: string[] = [
   "https://nextui.org/images/card-example-4.jpeg",
@@ -24,28 +25,34 @@ const Carousel: React.FC = () => {
   return (
     <div className="w-full sm:w-[400px] md:w-[500px] relative mx-auto">
       <Card className="max-w-[600px] h-[200px] md:h-[300px] lg:h-[400px] mx-auto">
-        <Image
-          removeWrapper
-          alt="Card background"
-          className="z-0 w-full h-full object-cover"
-          src={images[currentIndex]}
-        />
-        <div className="absolute top-0 left-0 w-full h-full">
-          <Button
-            isIconOnly
-            className="absolute top-1/2 transform -translate-y-1/2 left-2 text-warning bg-foreground/30 rounded-full z-1"
-            onClick={prevImage}
-          >
-            <LeftArrow />
-          </Button>
-          <Button
-            isIconOnly
-            className="absolute top-1/2 transform -translate-y-1/2 right-2 text-warning bg-foreground/30 rounded-full z-1"
-            onClick={nextImage}
-          >
-            <RightArrow />
-          </Button>
-        </div>
+        <Swipe
+          onSwipeLeft={nextImage}
+          onSwipeRight={prevImage}
+          className="relative z-10 w-full h-full"
+        >
+          <Image
+            removeWrapper
+            alt="Card background"
+            className="z-0 w-full h-full object-cover"
+            src={images[currentIndex]}
+          />
+          <div className="absolute top-0 left-0 w-full h-full">
+            <Button
+              isIconOnly
+              className="absolute top-1/2 transform -translate-y-1/2 left-2 text-warning bg-foreground/30 rounded-full z-11"
+              onClick={prevImage}
+            >
+              <LeftArrow />
+            </Button>
+            <Button
+              isIconOnly
+              className="absolute top-1/2 transform -translate-y-1/2 right-2 text-warning bg-foreground/30 rounded-full z-11"
+              onClick={nextImage}
+            >
+              <RightArrow />
+            </Button>
+          </div>
+        </Swipe>
       </Card>
     </div>
   );
