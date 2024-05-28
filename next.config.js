b1269bc69f6a30f568cfd877841d9aa4bad8257f
moramaan <dmorahidalgo@gmail.com>
2024-05-28 22:30:28 +0200

    Auth0 changes to get access token on client side

--- next.config.js
+++ next.config.js
@@ -16,6 +16,7 @@ const nextConfig = {
     AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+    AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
   },
 };
 
