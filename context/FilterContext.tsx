b2d698022cf06be08b53da93fedeee5328b83699
moramaan <dmorahidalgo@gmail.com>
2024-05-29 14:26:42 +0200

    Set filtered products fetch according to backend v1

--- context/FilterContext.tsx
+++ context/FilterContext.tsx
@@ -1,34 +1,72 @@
+// import React, { createContext, useState, useContext, ReactNode } from "react";
+
+// interface Filters {
+//   minPrice: number | null;
+//   maxPrice: number | null;
+//   categories: string[];
+//   condition: string;
+//   state: string;
+//   city: string;
+//   publishedSince: string;
+// }
+
+// interface FilterContextProps {
+//   filters: Filters;
+//   setFilters: (filters: Filters) => void;
+// }
+
+// const defaultFilters: Filters = {
+//   minPrice: 0,
+//   maxPrice: 9999999999,
+//   categories: [],
+//   condition: "",
+//   state: "",
+//   city: "",
+//   publishedSince: "",
+// };
+
+// const FilterContext = createContext<FilterContextProps | undefined>(undefined);
+
+// export const FilterProvider = ({ children }: { children: ReactNode }) => {
+//   const [filters, setFilters] = useState<Filters>(defaultFilters);
+
+//   return (
+//     <FilterContext.Provider value={{ filters, setFilters }}>
+//       {children}
+//     </FilterContext.Provider>
+//   );
+// };
+
+// export const useFilterContext = () => {
+//   const context = useContext(FilterContext);
+//   if (!context) {
+//     throw new Error("useFilterContext must be used within a FilterProvider");
+//   }
+//   return context;
+// };
+
+//v2
+// context/FilterContext.tsx
 import React, { createContext, useState, useContext, ReactNode } from "react";
 
-interface Filters {
-  minPrice: number | null;
-  maxPrice: number | null;
-  categories: string[];
-  condition: string;
-  state: string;
-  city: string;
-  publishedSince: string;
+export interface Filter {
+  column: string;
+  value: string;
+  min?: number;
+  max?: number;
+  orderBy?: string;
+  order?: string;
 }
 
 interface FilterContextProps {
-  filters: Filters;
-  setFilters: (filters: Filters) => void;
+  filters: Filter[];
+  setFilters: (filters: Filter[]) => void;
 }
-
-const defaultFilters: Filters = {
-  minPrice: 0,
-  maxPrice: 9999999999,
-  categories: [],
-  condition: "",
-  state: "",
-  city: "",
-  publishedSince: "",
-};
 
 const FilterContext = createContext<FilterContextProps | undefined>(undefined);
 
 export const FilterProvider = ({ children }: { children: ReactNode }) => {
-  const [filters, setFilters] = useState<Filters>(defaultFilters);
+  const [filters, setFilters] = useState<Filter[]>([]);
 
   return (
     <FilterContext.Provider value={{ filters, setFilters }}>
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,8 +1,130 @@
+// import FilterCard from "@/components/filter-card";
+// import { subtitle, title } from "@/components/primitives";
+// import SearchLayout from "@/layouts/search-layout";
+// import dynamic from "next/dynamic";
+// import { useEffect, useState } from "react";
+// import { useProductContext } from "@/context/ProductContext";
+// import { Product } from "@/types/index";
+// import { useFilterContext } from "@/context/FilterContext";
+// const { v4: uuidv4 } = require("uuid");
+
+// // dynamic import with SSR disabled due to hydration error
+// const ProductCard = dynamic(() => import("@/components/product-card"), {
+//   ssr: false,
+// });
+
+// //function to find the higher price
+// function findMaxValue(products: Product[]) {
+//   let maxValue = 0;
+//   products.forEach((product: Product) => {
+//     if (product.price > maxValue) {
+//       maxValue = product.price;
+//     }
+//   });
+//   return maxValue;
+// }
+
+// export default function SearchPage() {
+//   const { products, setProducts } = useProductContext();
+//   const [loading, setLoading] = useState<boolean>(true);
+//   const [error, setError] = useState<Error | null>(null);
+//   const [maxValue, setMaxValue] = useState<number>(1000);
+//   const [isFilterOpen, setIsFilterOpen] = useState<boolean>(false);
+//   const { filters } = useFilterContext();
+
+//   //first fetch of products
+//   useEffect(() => {
+//     fetchProductData();
+//   }, []);
+
+//   const fetchProductData = async () => {
+//     try {
+//       const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+//       //TODO: FETCH FILTERED PRODUCTS
+//       const response = await fetch(`${apiUrl}/items`);
+//       if (!response.ok) {
+//         throw new Error("Failed to fetch product data");
+//       }
+//       const jsonData = await response.json();
+//       setProducts(jsonData); //TODO change the assignment with fetched products
+//       setMaxValue(findMaxValue(jsonData));
+//       setLoading(false);
+//     } catch (error: any) {
+//       setError(error as Error);
+//       setLoading(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchProductData();
+//   }, [filters]);
+
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (error) {
+//     return <div>Error: {error.message}</div>;
+//   }
+
+//   return (
+//     <SearchLayout>
+//       <section className="flex flex-col items-center justify-center gap-4 pt-8 md:pt-12">
+//         <div className="inline-block max-w-2xl text-center justify-center">
+//           <h2 className={title({ color: "blue" })}>Búsqueda de Productos</h2>
+//           <h4 className={subtitle({ class: "mt-4" })}>
+//             ¡Encuentra lo que necesitas!
+//           </h4>
+//         </div>
+//       </section>
+//       <section className="flex flex-col items-center justify-center py-4">
+//         <div className="grid grid-cols-12 gap-5 max-w-[1400px] w-full text-center items-center justify-between font-mono text-sm sm:mt-5">
+//           <div className="lg:col-span-3 h-full hidden lg:block">
+//             <FilterCard maxValue={maxValue} isModal={false} setIsFilterOpen={setIsFilterOpen} />
+//             {/* <DynamicFilterCard maxValue={maxValue} /> */}
+//           </div>
+//           <div className="col-span-12 lg:col-span-9 h-full">
+//             <div className="flex justify-end mb-4 lg:hidden">
+//               <button
+//                 className="bg-blue-500 text-white px-4 py-2 rounded"
+//                 onClick={() => setIsFilterOpen(true)}
+//               >
+//                 Show Filters
+//               </button>
+//             </div>
+//             {isFilterOpen && (
+//               <div className="z-50 fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center lg:hidden">
+//                 <div className="relative">
+//                   <FilterCard maxValue={maxValue} isModal setIsFilterOpen={setIsFilterOpen} />
+//                   {/* <DynamicFilterCard maxValue={maxValue} /> */}
+//                   <button
+//                     className="absolute top-5 right-5 z-10 bg-red-500 text-white px-4 py-2 rounded mb-4"
+//                     onClick={() => setIsFilterOpen(false)}
+//                   >
+//                     Close
+//                   </button>
+//                 </div>
+//               </div>
+//             )}
+//             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+//               {products.map((data) => (
+//                 <ProductCard key={data.id} {...data} />
+//               ))}
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//     </SearchLayout>
+//   );
+// }
+
+//test filters work
+// pages/search.tsx
+import React, { useState, useEffect } from "react";
 import FilterCard from "@/components/filter-card";
 import { subtitle, title } from "@/components/primitives";
 import SearchLayout from "@/layouts/search-layout";
 import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
 import { useProductContext } from "@/context/ProductContext";
 import { Product } from "@/types/index";
 import { useFilterContext } from "@/context/FilterContext";
@@ -30,23 +152,25 @@ export default function SearchPage() {
   const [error, setError] = useState<Error | null>(null);
   const [maxValue, setMaxValue] = useState<number>(1000);
   const [isFilterOpen, setIsFilterOpen] = useState<boolean>(false);
-  const { filters } = useFilterContext();
+  const { filters, setFilters } = useFilterContext(); // Accessing filters from context
 
-  //first fetch of products
-  useEffect(() => {
-    fetchProductData();
-  }, []);
-
-  const fetchProductData = async () => {
+  // Fetch products with filters applied
+  const fetchProductData = async (filter: boolean) => {
     try {
       const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-      //TODO: FETCH FILTERED PRODUCTS
-      const response = await fetch(`${apiUrl}/items`);
+      const response = await fetch(`${apiUrl}/items/search`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ filters }),
+      });
+
       if (!response.ok) {
         throw new Error("Failed to fetch product data");
       }
       const jsonData = await response.json();
-      setProducts(jsonData); //TODO change the assignment with fetched products
+      setProducts(jsonData); // Set the fetched products
       setMaxValue(findMaxValue(jsonData));
       setLoading(false);
     } catch (error: any) {
@@ -56,8 +180,15 @@ export default function SearchPage() {
   };
 
   useEffect(() => {
-    fetchProductData();
-  }, [filters]);
+    fetchProductData(true);
+  }, [filters]); // Trigger fetching whenever filters change
+
+  // Reset filters when the component is unmounted
+  useEffect(() => {
+    return () => {
+      setFilters([]); // Reset filters
+    };
+  }, []);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -80,8 +211,11 @@ export default function SearchPage() {
       <section className="flex flex-col items-center justify-center py-4">
         <div className="grid grid-cols-12 gap-5 max-w-[1400px] w-full text-center items-center justify-between font-mono text-sm sm:mt-5">
           <div className="lg:col-span-3 h-full hidden lg:block">
-            <FilterCard maxValue={maxValue} isModal={false} setIsFilterOpen={setIsFilterOpen} />
-            {/* <DynamicFilterCard maxValue={maxValue} /> */}
+            <FilterCard
+              maxValue={maxValue}
+              isModal={false}
+              setIsFilterOpen={setIsFilterOpen}
+            />
           </div>
           <div className="col-span-12 lg:col-span-9 h-full">
             <div className="flex justify-end mb-4 lg:hidden">
@@ -95,8 +229,11 @@ export default function SearchPage() {
             {isFilterOpen && (
               <div className="z-50 fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center lg:hidden">
                 <div className="relative">
-                  <FilterCard maxValue={maxValue} isModal setIsFilterOpen={setIsFilterOpen} />
-                  {/* <DynamicFilterCard maxValue={maxValue} /> */}
+                  <FilterCard
+                    maxValue={maxValue}
+                    isModal
+                    setIsFilterOpen={setIsFilterOpen}
+                  />
                   <button
                     className="absolute top-5 right-5 z-10 bg-red-500 text-white px-4 py-2 rounded mb-4"
                     onClick={() => setIsFilterOpen(false)}
